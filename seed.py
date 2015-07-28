c0430c6fb087aab1b2cb0ad9a7325e60901fa322
Hackbright Student <info@hackbrightacademy.com>
2015-07-28 13:02:28 -0700

    working on the login

--- seed.py
+++ seed.py
@@ -8,7 +8,8 @@ def load_users():
     """Load users from u.user into database."""
     users_file = open('./seed_data/u.user')
     for line in users_file:
-        user_data = line.split('|')
+        user_data = line.rstrip().split('|')
+        # print "zipcode=", "*", user_data[4], "*"
         individual_user = User(user_id=user_data[0], age=user_data[1], zipcode=user_data[4])
         db.session.add(individual_user)
     db.session.commit()
@@ -21,7 +22,7 @@ def load_movies():
 
     movies_file = open('./seed_data/u.item')
     for line in movies_file:
-        movie_data = line.split('|')
+        movie_data = line.rstrip().split('|')
         movie_title = (" ").join(movie_data[1].split(" ")[:-1])
         if movie_data[2] != "":
             parsed_datetime = datetime.datetime.strptime(movie_data[2], '%d-%b-%Y')
@@ -38,7 +39,7 @@ def load_ratings():
     """Load ratings from u.data into database."""
     ratings_file = open('./seed_data/u.data')
     for line in ratings_file:
-        ratings_data = line.split('\t')
+        ratings_data = line.rstrip().split('\t')
     
         individual_rating = Rating(movie_id=ratings_data[1], user_id=ratings_data[0], score=ratings_data[2])
 
--- server.py
+++ server.py
@@ -1,11 +1,9 @@
 """Movie Ratings."""
 
 from jinja2 import StrictUndefined
-
-from flask import Flask
 from flask_debugtoolbar import DebugToolbarExtension
-
-from model import connect_to_db, db
+from flask import Flask, render_template, redirect, request, flash, session, url_for
+from model import User, Rating, Movie, connect_to_db, db
 
 
 app = Flask(__name__)
@@ -22,13 +20,37 @@ app.jinja_env.undefined = StrictUndefined
 def index():
     """Homepage."""
 
-    return "<html><body>Placeholder for the homepage.</body></html>"
+    return render_template("homepage.html")
+
+@app.route("/users")
+def user_list():
+    """Show list of users."""
+
+    users = User.query.all()
+    return render_template("user_list.html", users=users)
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = User.query.filter_by(email=email, password=password).first()
+    if user:
+        flash("Logged in")
+        session['loggedin'] = user.user_id
+    else:
+        new_user = User(email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("New user has been added to database")
+        session['loggedin'] = new_user.user_id
+    return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
-    app.debug = True
+    app.debug = False
 
     connect_to_db(app)
 
